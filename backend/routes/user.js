ad004fe62118d6f6c2da4268a476b36a0950c63d
MananSharma12 <manan.sharma1205@gmail.com>
2024-04-14 19:19:10 +0530

    feat: add signup route and authentication middleware

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,47 @@
 const express = require("express")
-
+const z = require("zod")
+const { User } = require("../db")
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require("../config");
 const router = express.Router()
 
-router.get('/signup', (req, res) => {})
-router.get('/signin', (req, res) => {})
+const signupSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+})
+
+router.post('/signup', async (req, res) => {
+    const body = req.body
+
+    const {success} = signupSchema.safeParse(body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = User.findOne({
+        username: body.username
+    })
+
+    if(existingUser) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser = await User.create(body)
+
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, JWT_SECRET)
+
+    res.json({
+        message: "User created successfully",
+        token
+    })
+})
 
 module.exports = router
