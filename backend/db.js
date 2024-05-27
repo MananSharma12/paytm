af33a76b82143f69d3dc6d56a00ec5d1f107f298
MananSharma12 <manan.sharma1205@gmail.com>
2024-05-27 19:59:48 +0530

    refactor: added better schema settings

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,32 @@ const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://admin:*****5@cluster0.lka11u3.mongodb.net/")
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30,
+    },
 })
 
 const User = mongoose.model('User', userSchema)
