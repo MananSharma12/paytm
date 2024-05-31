e9d7a0555aec406dc861eea39aeeb0eeb59b2718
MananSharma12 <manan.sharma1205@gmail.com>
2024-05-31 17:07:33 +0530

    feat: add router, basic routes, tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { SignUp } from "./pages/SignUp.jsx";
+import { SignIn } from "./pages/SignIn.jsx";
+import { Dashboard } from "./pages/Dashboard.jsx";
+import { SendMoney } from "./pages/SendMoney.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/send",
+    element: <SendMoney />,
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
