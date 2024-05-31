6c33d658726b8ef6a5bc93229177ee361dcadc8a
MananSharma12 <manan.sharma1205@gmail.com>
2024-05-31 11:46:27 +0530

    feat: added route for funds transfer

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -13,4 +13,48 @@ router.get('/balance', authMiddleware, async (req, res) => {
     })
 })
 
+router.post('/transfer', authMiddleware, async (req, res) => {
+    const {amount, to} = req.body
+
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    if (account.balance < amount) {
+        res.status(400).json({
+            message: "Insufficient balance"
+        })
+    }
+
+    const toAccount = await Account.findOne({
+        userId: to
+    })
+
+    if (!toAccount) {
+        res.status(404).json({
+            message: "Invalid Account"
+        })
+    }
+
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: {
+            balance: -amount
+        }
+    })
+
+    await Account.updateOne({
+        userId: to
+    }, {
+        $inc: {
+            balance: amount
+        }
+    })
+
+    res.status(200).json({
+        message: "Transfer Successful"
+    })
+})
+
 module.exports = router;
