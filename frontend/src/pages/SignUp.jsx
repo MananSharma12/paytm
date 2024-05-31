0020e338fcf3ca4efd6d17eb5be7b7be8e11c18f
MananSharma12 <manan.sharma1205@gmail.com>
2024-05-31 23:38:43 +0530

    feat: Add heading and subheadings for Signup page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,13 @@
+import { Heading } from "../ui/Heading.jsx";
+import { Subheading } from "../ui/Subheading.jsx";
+
 export const SignUp = () => {
-    return (
-       <div>Signup page</div>
-    )
-}
+  return (
+    <div className="ml-auto mr-auto mt-16 bg-blue-100 w-screen sm:w-1/2">
+      <div className="text-center">
+        <Heading label={"Sign up"} />
+        <Subheading content={"Enter your information to create an account"} />
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/ui/Subheading.jsx
@@ -0,0 +1,3 @@
+export const Subheading = ({ content }) => {
+  return <div className="mt-2 text-slate-500text-sm ">{content}</div>;
+};
