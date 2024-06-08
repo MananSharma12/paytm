ab13b752a9dee76998df8ec5faf73176ed204e3d
MananSharma12 <manan.sharma1205@gmail.com>
2024-06-08 17:10:24 +0530

    feat: add Navigation component and balance on dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,32 @@
+import axios from "axios";
+
+import {useEffect, useState} from "react";
+
+import {Navbar} from "./Navbar";
+
 export const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/api/v1/account/balance", {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+            .then((response) => {
+                setBalance(response.data.balance);
+            });
+    }, []);
+
     return (
-        <div>Dashboard</div>
-    )
-}
+        <>
+            <Navbar/>
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <h1 className="mt-4 ml-auto mr-auto size">
+                    Your Balance ₹{balance}
+                </h1>
+            </div>
+        </>
+    );
+};
